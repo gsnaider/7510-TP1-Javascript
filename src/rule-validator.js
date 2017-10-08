d854fb7b0bb79b467f76a65d48fcb04a44035e7c
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 20:13:17 -0300

    Add rule validator tests

---
+++ src/rule-validator.js
@@ -0,0 +1,12 @@
+var RuleValidator = function () {
+
+    /**
+     * Returns true if the format of factString is a valid fact format, or false otherwise.
+     */
+    this.isValidRule = function (ruleString) {
+        return true;
+    }
+
+}
+
+module.exports = RuleValidator;
---
+++ test/rule-validator-test.js
@@ -0,0 +1,41 @@
+var assert = require("chai").assert;
+var should = require('should');
+
+var RuleValidator = require('../src/rule-validator');
+
+describe("RuleValidator", function () {
+
+    var ruleValidator = null;
+
+    beforeEach(function () {
+        ruleValidator = new RuleValidator();
+    });
+
+    describe('RuleValidator check if valid rules', function () {
+
+        it('isValidRule returns true for valid rules.', function () {
+            assert(ruleValidator.isValidRule("hijo(X, Y) :- varon(X), padre(Y, X)."));
+            assert(ruleValidator.isValidRule("hija(X, Y) :- mujer(X), padre(Y, X)."));
+            assert(ruleValidator.isValidRule("subtract(X, Y, Z) :- add(Y, Z, X)."));
+            assert(ruleValidator.isValidRule("subtract(X, Y, Z) :- add(Y, Z, X)."));
+            assert(ruleValidator.isValidRule("wet_floor(X) :- rain(X)."));
+        });
+
+        it('isValidRule returns false for invalid rules.', function () {
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y)."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- ."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon()."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon(X), ."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon(X), padre."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon(X), padre()."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon(X), padre(X, )."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X, Y) :- varon(X), padre(X, Y)"));
+            assert.isFalse(ruleValidator.isValidRule("hijo :- varon(X), padre(X, Y)."));
+            assert.isFalse(ruleValidator.isValidRule("hijo() :- varon(X), padre(X, Y)."));
+            assert.isFalse(ruleValidator.isValidRule("hijo(X) varon(X), padre(X, Y)."));
+        });
+
+    });
+
+});
