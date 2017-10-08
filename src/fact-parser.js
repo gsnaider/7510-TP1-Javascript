c13ba0362053e258911b3971fb7012f44d17c764
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 13:50:35 -0300

    Add parseFacts logic

--- src/fact-parser.js
+++ src/fact-parser.js
@@ -1,8 +1,26 @@
+var FactValidator = require('../src/fact-validator');
+
 var FactParser = function () {
 
+  const RULE_ASSIGN_CODE = ":-";
+  var validator = new FactValidator();
+
+  var parseFact = function(factString) {
+    if (!validator.isValidFact(factString)) {
+      throw new Error("Invalid fact.");
+    }
+
+  }
+
   this.parseFacts = function (database) {
-    return new Set();
-    //throw new Error("Incomplete database");
+    var facts = new Set();
+    for (var i = 0; i < database.length; i++) {
+      var line = database[i];
+      if (!line.includes(RULE_ASSIGN_CODE)) {
+        facts.add(parseFact(line));
+      }
+    }
+    return facts;
   }
 
 }
