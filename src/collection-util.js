c5783506c50b36b473dec0fef947ff78154f06a1
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 16:57:40 -0300

    Add collection-util

---
+++ src/collection-util.js
@@ -0,0 +1,31 @@
+var CollectionUtil = function () {
+
+    /**
+     * Returns true if both arrays are equal, that is, they have the same
+     * elements in the same order, or false otherwise.
+     */
+    this.equalArrays = function (a1, a2) {
+        return (a1.length == a2.length && a1.every(function (v, i) {
+            return v === a2[i]
+        }));
+    }
+
+    /**
+     * Returns true if both Sets are equal, that is, they have the same
+     * elements, or false otherwise.
+     */
+    this.equalSets = function (s1, s2) {
+        if (s1.size !== s2.size) {
+            return false;
+        }
+        for (var a of s1) {
+            if (!(s2.has(a))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+module.exports = CollectionUtil;
