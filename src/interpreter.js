4c57430f5b6e693c4928addf2da4a4eaccfbb07a
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 18:34:40 -0300

    Use query parser in interpreter

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,20 +1,29 @@
 var FactParser = require('../src/fact-parser');
+var QueryParser = require('../src/query-parser');
 
 var Interpreter = function () {
 
     var facts = null;
     var factParser = new FactParser();
+    var queryParser = new QueryParser();
+
 
     this.parseDB = function (database) {
         try {
             facts = factParser.parseFacts(database);
         } catch (error) {
-            console.error(error.message);
+            console.error("Error parsing database: " + error.message);
         }
     }
 
-    this.checkQuery = function (params) {
+    this.checkQuery = function (inputQuery) {
         if (!facts) {
+            return null;
+        }
+        try {
+            var query = queryParser.parseQuery(inputQuery);
+        } catch (error) {
+            console.error(error.message);
             return null;
         }
         return true;
