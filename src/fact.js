f447506f1ccf1acc0bfe43985eaecc404d19367b
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 13:14:18 -0300

    Create fact model

---
+++ src/fact.js
@@ -0,0 +1,16 @@
+var Fact = function (name, params) {
+
+    this.name = name;
+    this.params = params;
+
+    this.getName = function () {
+      return this.name;
+    }
+
+    this.getParams = function () {
+      return this.params;
+    }
+
+}
+
+module.exports = Fact;
