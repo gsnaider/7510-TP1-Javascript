a4cf7916a747ab748f4b0b27a7b7f3fda0df3abb
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 16:58:58 -0300

    Complete fact parser logic

--- src/fact-parser.js
+++ src/fact-parser.js
@@ -1,20 +1,28 @@
 var FactValidator = require('../src/fact-validator');
 var ParserUtil = require('../src/parser-util');
-var Fact = require('../src/fact')
 
 var FactParser = function () {
 
     const RULE_ASSIGN_CODE = ":-";
+    const END_LINE_REGEX = /\.$/;
     var validator = new FactValidator();
     var parserUtil = new ParserUtil();
 
+    /**
+     * Returns the fact as a string without whitespace and without the end-line character,
+     * or throws an Error if the fact has an invalid format.
+     */
     var parseFact = function (factString) {
         if (!validator.isValidFact(factString)) {
             throw new Error("Invalid fact.");
         }
-        return new Fact(parserUtil.parseName(factString), parserUtil.parseParams(factString));
+        return parserUtil.removeWhitespace(factString).replace(END_LINE_REGEX, "");
     }
 
+    /**
+     * Returns a set containing all the facts from the database as strings,
+     * or throws an Exception if the databse is invalid.
+     */
     this.parseFacts = function (database) {
         var facts = new Set();
         for (var i = 0; i < database.length; i++) {
--- test/fact-parser-test.js
+++ test/fact-parser-test.js
@@ -3,58 +3,46 @@ var should = require('should');
 var assert = require('assert');
 
 var FactParser = require('../src/fact-parser');
-var Fact = require('../src/fact')
+var CollectionUtil = require('../src/collection-util');
 
 describe("FactParser", function () {
 
-  var parentDatabase = [
-    "varon(juan).",
-    "padre(juan, pepe).",
-    "hijo(X, Y) :- varon(X), padre(Y, X).",
-    "hija(X, Y) :- mujer(X), padre(Y, X)."
-  ];
+    var parentDatabase = [
+        "varon(juan).",
+        "padre(juan, pepe).",
+        "hijo(X, Y) :- varon(X), padre(Y, X).",
+        "hija(X, Y) :- mujer(X), padre(Y, X)."
+    ];
 
-  var incompleteDatabase = [
-    "varon(juan).",
-    "varon"
-  ];
+    var incompleteDatabase = [
+        "varon(juan).",
+        "varon"
+    ];
 
-  var factParser = null;
+    var factParser = null;
+    var collectionUtil = new CollectionUtil();
 
-  beforeEach(function () {
-    // runs before each test in this block
-    factParser = new FactParser();
-  });
-
-  describe('FactParser parse facts', function () {
-
-    it('parseFacts returns a set of Facts from a valid database.', function () {
-      assert(equalSets(factParser.parseFacts(parentDatabase),
-        new Set([
-          new Fact("varon", ["juan"]),
-          new Fact("varon", ["juan", "pepe"])])));
-            
+    beforeEach(function () {
+        // runs before each test in this block
+        factParser = new FactParser();
     });
 
-    it('parseFacts throws Exception with an incomplete database.', function () {
-      assert.throws(
-        function() { factParser.parseFacts(incompleteDatabase); }, 
-        Error);
+    describe('FactParser parse facts', function () {
+
+        it('parseFacts returns a set of facts from a valid database.', function () {
+            assert(collectionUtil.equalSets(
+                factParser.parseFacts(parentDatabase),
+                new Set(["varon(juan)", "padre(juan,pepe)"])));
+        });
+
+        it('parseFacts throws Exception with an incomplete database.', function () {
+            assert.throws(
+                function () {
+                    factParser.parseFacts(incompleteDatabase);
+                },
+                Error);
+        });
+
     });
-
-  });
-
-
-  function equalSets(s1, s2) {
-    if (s1.size !== s2.size) {
-      return false;
-    }
-    for (var a of s1) {
-      if (!s2.has(a)) {
-        return false;
-      }
-    }
-    return true;
-  }
 
 });
