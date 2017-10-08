cde0aef038d3a5880e02523a6a5c6f3c386af1a1
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 13:29:21 -0300

    Add skeleton for fact parser

---
+++ src/fact-parser.js
@@ -0,0 +1,10 @@
+var FactParser = function () {
+
+  this.parseFacts = function (database) {
+    return new Set();
+    //throw new Error("Incomplete database");
+  }
+
+}
+
+module.exports = FactParser;
--- src/fact.js
+++ src/fact.js
@@ -1,15 +1,15 @@
 var Fact = function (name, params) {
 
-    this.name = name;
-    this.params = params;
+  this.name = name;
+  this.params = params;
 
-    this.getName = function () {
-      return this.name;
-    }
+  this.getName = function () {
+    return this.name;
+  }
 
-    this.getParams = function () {
-      return this.params;
-    }
+  this.getParams = function () {
+    return this.params;
+  }
 
 }
 
