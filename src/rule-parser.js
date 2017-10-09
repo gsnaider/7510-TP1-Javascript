85d0e0d3c51fb797b9d41d5e49abdf3eb114db27
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 21:45:59 -0300

    Add general logic for rule parsing

--- src/rule-parser.js
+++ src/rule-parser.js
@@ -1,11 +1,49 @@
+var RuleValidator = require('../src/rule-validator');
+var ParserUtil = require('../src/parser-util');
+var Rule = require('../src/rule');
+
 var RuleParser = function () {
 
+    const RULE_ASSIGN_CODE = ":-";
+    var validator = new RuleValidator();
+    var parserUtil = new ParserUtil();
+
     /**
-     * Returns a set of Rules containing all the rules from the database,
+     * Returns a set containing all the facts as strings from a ruleString.
+     */
+    var parseRuleFacts = function (ruleString) {
+        //TODO
+        return new Set();
+    }
+
+    /**
+     * Converts a ruleString to a Rule, or throws an Exception if the ruleString is invalid.
+     */
+    var parseRule = function (ruleString) {
+        if (!validator.isValidRule(ruleString)) {
+            throw new Error("Invalid rule: " + ruleString);
+        }
+        var rule = new Rule(parserUtil.parseName(ruleString),
+            parserUtil.parseParams(ruleString),
+            parseRuleFacts(ruleString));
+        // TODO: Validate rule params.
+    }
+
+
+    /**
+     * Returns a map of Rules, with their name as key, containing all the rules from the database,
      * or throws an Exception if the databse is invalid.
      */
     this.parseRules = function (database) {
-        return Set([]);
+        var rules = {};
+        for (var i = 0; i < database.length; i++) {
+            var line = database[i];
+            if (line.includes(RULE_ASSIGN_CODE)) {
+                var rule = parseRule(line);
+                rules[rule.getName()] = rule;
+            }
+        }
+        return rules;
     }
 
 }
