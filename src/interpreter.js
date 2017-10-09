7771ec5d72363f656b07a471f902ae766298ccb5
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 23:02:45 -0300

    Use rule parser in interpreter

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,20 +1,21 @@
 var FactParser = require('../src/fact-parser');
+var RuleParser = require('../src/rule-parser');
 var QueryParser = require('../src/query-parser');
 var Database = require('../src/database');
 
 var Interpreter = function () {
 
-    var facts = null;
     var database = null
     var factParser = new FactParser();
+    var ruleParser = new RuleParser();
     var queryParser = new QueryParser();
-
 
     this.parseDB = function (inputDatabase) {
         try {
-            facts = factParser.parseFacts(inputDatabase);
-            // TODO: parse rules and set to database.
-            database = new Database(facts, {});
+            database =
+                new Database(
+                    factParser.parseFacts(inputDatabase),
+                    ruleParser.parseRules(inputDatabase));
         } catch (error) {
             console.error("Error parsing database: " + error.message);
         }
