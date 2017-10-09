00d11ca39f607d8e0a2de6befde8127ae813628b
Gaston Snaider <gastonsnaider93@gmail.com>
2017-10-08 22:13:17 -0300

    Add rule-params check tests

--- src/rule-validator.js
+++ src/rule-validator.js
@@ -12,6 +12,17 @@ var RuleValidator = function () {
         return RULE_REGEX.test(parserUtil.removeWhitespace(ruleString));
     }
 
+    /**
+     * Returns true if the parameters declared in the rule name are the same as
+     * the parameters in its facts, or false otherwise.
+     * @param rule
+     * @returns {boolean}
+     */
+    this.ruleHasValidParams = function (rule) {
+        // TODO
+        return true;
+    }
+
 }
 
 module.exports = RuleValidator;
--- test/rule-validator-test.js
+++ test/rule-validator-test.js
@@ -1,6 +1,7 @@
 var assert = require("chai").assert;
 var should = require('should');
 
+var Rule = require('../src/rule');
 var RuleValidator = require('../src/rule-validator');
 
 describe("RuleValidator", function () {
@@ -35,6 +36,46 @@ describe("RuleValidator", function () {
             assert.isFalse(ruleValidator.isValidRule("hijo() :- varon(X), padre(X, Y)."));
             assert.isFalse(ruleValidator.isValidRule("hijo(X) varon(X), padre(X, Y)."));
         });
+
+    });
+
+
+    describe('RuleValidator check if valid rule params', function () {
+
+        it('ruleHasValidParams returns true for rules with the same parameters in its name and in its facts.',
+            function () {
+                assert(ruleValidator.ruleHasValidParams(
+                    new Rule(
+                        "hijo",
+                        ["X", "Y"],
+                        new Set(["varon(X)", "padre(X, Y)"]))));
+                assert(ruleValidator.ruleHasValidParams(
+                    new Rule(
+                        "subtract",
+                        ["X", "Y", "Z"],
+                        new Set(["add(Y, Z, X)"]))));
+                assert(ruleValidator.ruleHasValidParams(
+                    new Rule(
+                        "wet_floor",
+                        ["X"],
+                        new Set(["rain(X)"]))));
+            });
+
+        it('ruleHasValidParams returns false for rules with different parameters in its name than in its facts.',
+            function () {
+                assert.isFalse(ruleValidator.ruleHasValidParams(
+                    new Rule("hijo", ["X"], new Set(["varon(Y)"]))));
+                assertisFalse(ruleValidator.ruleHasValidParams(
+                    new Rule(
+                        "hijo",
+                        ["X"],
+                        new Set(["varon(X)", "padre(X, Y)"]))));
+                assertisFalse(ruleValidator.ruleHasValidParams(
+                    new Rule(
+                        "hijo",
+                        ["X", "Y"],
+                        new Set(["varon(Y)"]))));
+            });
 
     });
 
